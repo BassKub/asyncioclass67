05c5327015469f83fe3f0c1cfa416bbb796fbddb
Suparoek Grudsorn <87462178+BassKub@users.noreply.github.com>
2024-07-12 10:13:34 +0700

    Done asyncio03

--- assignment5/asyncio03.py
+++ assignment5/asyncio03.py
@@ -1,1 +1,22 @@
 # example of waiting for the first task to fail
+# example of waiting for the first task to complete
+# example of waiting for all tasks to complete
+from random import random
+import asyncio
+
+async def task_coro(arg):
+    value = random()
+    await asyncio.sleep(value)
+    print(f'>task {arg} done with {value}')
+    if value < 0.5:
+        raise Exception(f'Somerhing bad happend in {arg}')
+
+async def main():
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    print('All done')
+    first = done.pop()
+    print(first)
+
+
+asyncio.run(main())
